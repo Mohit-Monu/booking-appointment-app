d2f8588254a7bb72ee2bcbbe7993cbadbff773a8
mohitr Agarwal <bmohit700@gmail.com>
2023-05-18 23:32:10 +0530

    some more implementation

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express=require('express')
 const path=require('path')
 const bodyParser = require("body-parser");
+const cors = require('cors')
 
 const db=require('./utils/database')
 const adminRoutes=require('./routes/admin')
 
 
 const app = express();
+app.use(cors())
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(adminRoutes);
